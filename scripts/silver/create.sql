11984e86b417f22f8c124c95dc26eb6d59586674
Rahul Bhagat <rahulkrchaurasiya125@gmail.com>
2026-05-10 20:53:03 +0530

    comit

---
+++ scripts/silver/create.sql
@@ -0,0 +1,70 @@
+IF OBJECT_ID('silver.crm_cust_info','U') Is NOT NULL
+	Drop table silver.crm_cust_info;
+
+GO
+
+create table silver.crm_cust_info (
+cst_id Int,
+csr_key nvarchar(50),
+csr_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_marital_status nvarchar(50),
+cst_gnder nvarchar(50),
+cst_create_date date,
+dwh_create_date    DATETIME2 DEFAULT GETDATE()
+);
+
+
+GO
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
